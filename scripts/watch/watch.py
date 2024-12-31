999becc6ec90ab2379243ce743301f4ffd87811b
Pedro Fuenmayor <p.fuenmayor@ampeersenergy.de>
2024-12-31 13:33:04 +0100

    refactor: watch file

--- scripts/watch/watch.py
+++ scripts/watch/watch.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -6,12 +7,21 @@ import os
 
 class PyFileHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        if event.src_path.endswith('sum_of_two_numbers.py'):
-            print("\nFile changed! Running sum_of_two_numbers.py...")
-            subprocess.run(['python', 'sum_of_two_numbers.py'], cwd=os.path.dirname(__file__))
+        if event.src_path.endswith(self.file_name):
+            print(f"\nFile changed! Running {self.file_name}...")
+            subprocess.run(['python', self.file_name], cwd=os.path.dirname(__file__))
+
+    def __init__(self, file_name):
+        super().__init__()
+        self.file_name = file_name
 
 if __name__ == "__main__":
-    event_handler = PyFileHandler()
+    if len(sys.argv) < 2:
+        print("Usage: python watch.py <file_name>")
+        sys.exit(1)
+
+    file_to_watch = sys.argv[1]
+    event_handler = PyFileHandler(file_to_watch)
     observer = Observer()
     observer.schedule(event_handler, path=os.path.dirname(__file__), recursive=False)
     observer.start()
