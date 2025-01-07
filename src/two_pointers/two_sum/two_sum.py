12d5efc882350f50f5b4369e8c284b02c9117a59
Pedro Fuenmayor <p.fuenmayor@ampeersenergy.de>
2025-01-07 09:38:59 +0100

    feat: two sum brut force

--- src/two_pointers/two_sum/two_sum.py
+++ src/two_pointers/two_sum/two_sum.py
@@ -11,9 +11,18 @@ Examples:
 """
 
 
-def two_sum(first_number, second_number):
-    return first_number + second_number
+def two_sum_v1(nums, target):
+    first_idx = 0
+    last_idx = len(nums)
+
+    for outer_idx in range(first_idx, last_idx):
+        inner_start_idx = outer_idx + 1
+        for inner_idx in range(inner_start_idx, last_idx):
+            if nums[outer_idx] + nums[inner_idx] == target:
+                return True
+
+    return False
 
 
 if __name__ == "__main__":
-    print(two_sum(5, 5))
+    print(f"Result: {two_sum_v1([1,3,4,6,8,10,13], 13)}")
