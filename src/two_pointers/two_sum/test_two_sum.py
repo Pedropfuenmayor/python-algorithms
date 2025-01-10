f106021bda864521112efb2fd535da32b3f41ead
Pedro Fuenmayor <p.fuenmayor@ampeersenergy.de>
2025-01-10 09:14:05 +0100

    test: add missing test case for two sum

--- src/two_pointers/two_sum/test_two_sum.py
+++ src/two_pointers/two_sum/test_two_sum.py
@@ -23,3 +23,7 @@ def test_target_greater_than_maximal_sum():
 
 def test_target_found_v2():
     assert two_sum_v2([1, 3, 4, 6, 8, 10, 13], 13) is True
+
+
+def test_target_not_found_v2():
+    assert two_sum_v2([1, 3, 4, 6, 8, 10, 13], 6) is False
